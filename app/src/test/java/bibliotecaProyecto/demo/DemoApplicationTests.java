6a8ca0fe0062e147ce6720cb8c4b72b1ec96b186
JuanOliveros2497 <oliverosduranj@gmail.com>
2026-05-16 17:41:07 -0500

    feat: agrego estructura base Spring Boot

---
+++ app/src/test/java/bibliotecaProyecto/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bibliotecaProyecto.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
